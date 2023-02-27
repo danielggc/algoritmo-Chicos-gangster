f2bc5fa6745d6883b2ba9b6d8166e88cfce36477
dgc7 <dgc7@dgc7.dgc7>
2023-02-27 13:38:55 -0500

    algoritmo imlementado con un arbol  binario

---
+++ README.md
@@ -0,0 +1,3 @@
+## algoritmos chicos ganster
+ingrese un valor a la función y está devolverá la cantidad de cada cara y su respectivo 
+string de emojis  
--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include <stdlib.h> 
 #include <errno.h> 
 #include <string>
-#include <strings.h>
 #include <sys/select.h>
 #include <unistd.h>   //close 
 #include <arpa/inet.h>    //close 
@@ -26,12 +25,88 @@ const char * emoji_completo      = "(-_-)";
 const char * emoji_final_i        = "(-";
 const char * emoji_lateral_i      = "(-_";
 const char * emoji_parcial_i      = "(-_-";
+
 const int size_emoji_final      = 2;
 const int size_emoji_lateral    = 3;
 const int size_emoji_parcial    = 4;
 const int size_emoji_completo   = 5;
 
-void calcular_chicos_gamster( int catidad_chicos ){
+using namespace std;
+
+
+enum Address{  Letf , Rigth };
+
+typedef string  Elemento ;
+
+
+struct Nodo{
+      public:
+        Elemento elemento;
+        Nodo *rigth;      
+        Nodo *letft;    
+
+
+  };
+
+
+
+
+
+struct Nodo *  create_new_nodo( Elemento elemento ){
+    
+    int size_nodo = sizeof( struct Nodo );
+    Nodo *  nuevo_nodo  =  ( struct Nodo* )malloc( size_nodo );
+    nuevo_nodo -> elemento = elemento;
+    nuevo_nodo -> rigth =     nuevo_nodo -> letft  = NULL;
+ 
+    return nuevo_nodo;
+}
+
+ 
+void  insert_to_tree( struct Nodo  *&nodo ,  Elemento elemento ){
+    
+    if(  nodo -> elemento < elemento ){
+        
+        if( nodo -> rigth == NULL ){
+            nodo -> rigth = create_new_nodo( elemento );
+        }
+        else{
+            insert_to_tree( nodo -> rigth , elemento);
+        }
+    }
+    else{
+
+        if( nodo -> letft == NULL ){
+        
+            nodo -> letft  = create_new_nodo( elemento );
+
+        }
+        else{
+            insert_to_tree( nodo -> letft,  elemento );
+        }
+    }
+
+ }
+void print_emogis( Nodo * root_tree ){
+    if( root_tree ==  NULL ){
+      return;
+    }
+    else{
+        print_emogis( root_tree->letft );
+        string data = root_tree->elemento;
+        cout<<data;
+        if( root_tree -> rigth == NULL) 
+            return;
+    }
+    while ( root_tree -> rigth != NULL ) {
+        root_tree = root_tree->rigth;
+        cout<<root_tree->elemento;
+    }
+    
+
+}
+
+void calcular_chicos_gamster( int gangster_boys ){
    
 
     int num_emoji_final = 0;
@@ -39,12 +114,12 @@ void calcular_chicos_gamster( int catidad_chicos ){
     int num_emoji_parcial = 0;
     const int num_emoji_completo = 1;
 
-    if( catidad_chicos > 7 ){
+    if( gangster_boys > 7 ){
         num_emoji_final = 2;
     }
     int i = 1;
     int pares = 0;
-    for( ; i < catidad_chicos   ; i++){
+    for( ; i < gangster_boys   ; i++){
         if(pares == 4 ){
             num_emoji_parcial++;
             pares =0;
@@ -55,95 +130,71 @@ void calcular_chicos_gamster( int catidad_chicos ){
         }
     }
   
-    num_emoji_lateral = ( catidad_chicos - ( num_emoji_completo + num_emoji_final + num_emoji_parcial ) );
+    num_emoji_lateral = ( gangster_boys - ( num_emoji_completo + num_emoji_final + num_emoji_parcial ) );
 
     printf("emoji_final = [%d], emoji_lateral = [%d] ,  emoji_parcial = [%d], emoji_completo = [%d]", num_emoji_final, num_emoji_lateral, num_emoji_parcial, num_emoji_completo );
 
-    const int espacios_final  =  ( num_emoji_final  ) * size_emoji_final;
-    const int espacios_lateral = num_emoji_lateral * size_emoji_lateral ;
-    const int espacios_parcial = num_emoji_parcial * size_emoji_parcial ;
-
-    const int espacios_total = espacios_lateral + size_emoji_completo + espacios_parcial;
-    char  data [ espacios_total + 10] ;
     
-
-    memset( data, '\0', espacios_total + 10 );
-
-    int count = 0;
-
-    if ( catidad_chicos > 7) {
-        strcat(data, emoji_final_i);
-        count++;
-    }
+    Nodo *  listEmojis =  create_new_nodo(emoji_completo);
     
+    int count =1 ;
+    Address following = Letf;
+    const int end_race  = gangster_boys > 7 ? 2 : 0;
+    Nodo * aux_letft_nodo = listEmojis;
+    Nodo * auc_rigth_nodo = listEmojis;
 
-    const int  mitad = catidad_chicos % 2 == 0 ? catidad_chicos / 2 : (catidad_chicos / 2)+ 1  ; 
-    
-    int izq_num_laterales = num_emoji_lateral % 2 == 0 ? num_emoji_lateral / 2  : ( num_emoji_lateral / 2 ) + 1;
-    const int izq_num_parciales = num_emoji_parcial % 2 == 0 ? num_emoji_parcial / 2  : ( num_emoji_parcial / 2 ) + 1;
-
-    if( num_emoji_parcial !=  0  ){
-        while( izq_num_laterales    != ( izq_num_parciales * 2 ) ) {
-            strcat(data, emoji_lateral_i);
-            izq_num_laterales -= 1;
-            count++;
-        }    
-        strcat(data, emoji_parcial_i);
-        count ++;
-    }
-    int toletf = catidad_chicos > 7 ? 1: 0;
-    int count_pares = 0;
-    while ( count <( mitad -toletf) ) {
-        if( count_pares  == 2 && num_emoji_parcial != 0  ){
-            strcat(data, emoji_parcial_i);
-            count_pares = 0;
-            count ++;
+ 
+ 
+    while( count < ( gangster_boys - end_race  ) ){
+        if( following == Letf ){
+            printf("\nizquierda   =>%d",count);     
+            if( ( count - 1 ) % 4 == 0 & count > 1   ){
+                Nodo * newEmogi = create_new_nodo( emoji_parcial_i );
+                aux_letft_nodo->letft =  newEmogi;
+                aux_letft_nodo = aux_letft_nodo -> letft; 
+                count++;
+                following = Rigth;
+                printf("emoji parcial ");
+            }else {
+                Nodo * newEmogi = create_new_nodo( emoji_lateral_i );
+                aux_letft_nodo->letft =  newEmogi;
+                aux_letft_nodo = aux_letft_nodo -> letft; 
+                count++;
+                following = Rigth;
+                printf(" emoji lateral ");
+            }
         }
         else{
-            strcat(data, emoji_lateral_i);
-            count ++;
-            count_pares++;
+            printf("\nderecha   => "); 
+            if( ( count - 2 ) % 4 == 0   & count > 2 ){
+                Nodo * newEmogi = create_new_nodo( emoji_parcial_d );
+                auc_rigth_nodo->rigth =  newEmogi;
+                auc_rigth_nodo = auc_rigth_nodo->rigth;
+                count++;
+                following = Letf;
+                printf("emoji parciar ");
+            }
+            else {
+                Nodo * newEmogi = create_new_nodo( emoji_lateral_d );
+                auc_rigth_nodo->rigth =  newEmogi;
+                auc_rigth_nodo = auc_rigth_nodo->rigth;
+                count++;
+                following = Letf;   
+                printf("emoji lateral "); 
+            }  
         }
-    }
+      }
 
-    strcat(data, emoji_completo);
-    count_pares = 0;
-    if ( catidad_chicos > 7) {
-        
-        while ( count < catidad_chicos -1 ) {
-            if( count_pares  == 2 &&  num_emoji_parcial != 0  ){
-                strcat(data, emoji_parcial_d);
-                count_pares = 0;
-                count ++;
-            }
-            else{
-                strcat(data, emoji_lateral_d);
-                count ++;
-                count_pares++;
-            }
-        }
-        strcat(data, emoji_final_d);
+      if( end_race > 0  ){
+            Nodo * newEmogi_z = create_new_nodo( emoji_final_i );
+            aux_letft_nodo->letft =  newEmogi_z;
+            Nodo * newEmogi_r = create_new_nodo( emoji_final_d );
+            auc_rigth_nodo->rigth =  newEmogi_r;     
+      }
 
-
-    }
-    else {
-            while ( count < catidad_chicos  -1 ) {
-            if( count_pares  == 2 && num_emoji_parcial != 0  ){
-                strcat(data, emoji_parcial_d);
-                count_pares = 0;
-                count ++;
-            }
-            else{
-                strcat(data, emoji_lateral_d);
-                count ++;
-                count_pares++;
-            }
-        }
-
-    }
-
-    printf("\n\n%s",data);
-
+    printf("\n------------------------------------------");
+    cout<<"\n\nimprimiendo emogis\n\n";
+    print_emogis( listEmojis );
     
 }
 
@@ -151,8 +202,7 @@ void calcular_chicos_gamster( int catidad_chicos ){
 
 
 
-
 int main(){
     printf("\nchicos gangster: \n \n \n");
-    calcular_chicos_gamster(14);
+    calcular_chicos_gamster( 11);
 }
